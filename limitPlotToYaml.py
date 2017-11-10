71549f89dba5b6388c4b9f34f24f4d7882cc2904
Knut Kiesel <kiesel@cern.ch>
2017-11-10 12:41:42 +0100

    also allow tgraph2d conversion

--- limitPlotToYaml.py
+++ limitPlotToYaml.py
@@ -21,18 +21,15 @@ def getFromFile(filename, objectname):
     h = ROOT.gROOT.CloneObject(h)
     return h
 
-def getH2():
-    limitFile = "$HOME/phd/plotter/limitCalculations/T5Wg_v11/saved_graphs1d_limit.root"
-    h2_name_observed = "obs_hist"
-    h2 = getFromFile(limitFile, h2_name_observed)
-    return h2
+def init_axis(title="title", unit=""):
+    return {"header": {"name": title, "units": unit}, "values": []}
 
 def th2_to_data(h2, xmin=-1, xmax=-1, ymin=-1, ymax=-1):
     data = [[],[],[]]
     for xbin in range(1, h2.GetNbinsX()+2):
         xbinMin = h2.GetXaxis().GetBinLowEdge(xbin)
         xbinMax = h2.GetXaxis().GetBinUpEdge(xbin)
-        if xmin>0 and xbinMin < xmin or xmax>0 and xmax>0 and xbinMax > xmax: continue
+        if xmin>0 and xbinMin < xmin or xmax>0 and xbinMax > xmax: continue
         for ybin in range(1, h2.GetNbinsY()+2):
             ybinMin = h2.GetYaxis().GetBinLowEdge(ybin)
             ybinMax = h2.GetYaxis().GetBinUpEdge(ybin)
@@ -42,6 +39,51 @@ def th2_to_data(h2, xmin=-1, xmax=-1, ymin=-1, ymax=-1):
             data[0].append({"low": xbinMin, "high": xbinMax})
             data[1].append({"low": ybinMin, "high": ybinMax})
             data[2].append({"value": content})
+    return data
+
+def th2_to_yaml(data, xAxis, yAxis, qualifiers=[], convert_to_fb=False):
+    h2yaml = {
+        "independent_variables": [xAxis, yAxis],
+        "dependent_variables": [{"header": {"name": "SIG", "units": "PB"}, "qualifiers": qualifiers, "values": []}]
+    }
+    h2yaml["independent_variables"][0]["values"] = data[0]
+    h2yaml["independent_variables"][1]["values"] = data[1]
+    h2yaml["dependent_variables"][0]["values"] = data[2]
+    if convert_to_fb:
+        h2yaml["dependent_variables"][0]["header"]["units"] = "FB"
+        h2yaml["dependent_variables"][0]["values"] = [{"value": 1000*d["value"]} for d in data[2]]
+    return h2yaml
+
+def tgraph2d_to_data(gr, xmin=-1, xmax=-1, ymin=-1, ymax=-1):
+    data = [[], [], []]
+    for n in range(gr.GetN()):
+        x, y = gr.GetX()[n], gr.GetY()[n]
+        if xmin>0 and x < xmin or xmax>0 and xmax>0 and x > xmax: continue
+        if ymin>0 and y < ymin or ymax>0 and ymax>0 and y > ymax: continue
+        data[0].append({"value": x})
+        data[1].append({"value": y})
+        data[2].append({"value": gr.GetZ()[n]})
+    data = [list(x) for x in zip(*sorted(zip(data[0], data[1], data[2]), key=lambda triple: triple[0]["value"]*1e6+triple[1]["value"]))]
+    return data
+
+def tgraph2d_to_yaml(data, xAxis, yAxis, qualifiers=[], convert_to_fb=False):
+    gr_yaml = {
+        "independent_variables": [xAxis, yAxis],
+        "dependent_variables": [{"header": {"name": "SIG", "units": "PB"}, "qualifiers": qualifiers, "values": []}]
+    }
+    gr_yaml["independent_variables"][0]["values"] = data[0]
+    gr_yaml["independent_variables"][1]["values"] = data[1]
+    gr_yaml["dependent_variables"][0]["values"] = data[2]
+    if convert_to_fb:
+        gr_yaml["dependent_variables"][0]["header"]["units"] = "FB"
+        gr_yaml["dependent_variables"][0]["values"] = [{"value": 1000*d["value"]} for d in data[2]]
+    return gr_yaml
+
+def tgraph_to_data(gr):
+    data = [[], []]
+    for n in range(gr.GetN()):
+        data[0].append({"value": round(gr.GetX()[n], 2)})
+        data[1].append({"value": round(gr.GetY()[n], 2)})
     return data
 
 def tgraph_to_yaml(data, xAxis, yAxis, qualifiers=[]):
@@ -54,27 +96,7 @@ def tgraph_to_yaml(data, xAxis, yAxis, qualifiers=[]):
     gr_yaml["dependent_variables"][0]["values"] = data[1]
     return gr_yaml
 
-def init_axis(title="title", unit=""):
-    return {"header": {"name": title, "units": unit}, "values": []}
-
-def tgraph_to_data(gr):
-    data = [[],[]]
-    for n in range(gr.GetN()):
-        data[0].append({"value": gr.GetX()[n]})
-        data[1].append({"value": gr.GetY()[n]})
-    return data
-
-def th2_to_yaml(data, xAxis, yAxis, qualifiers=[]):
-    h2yaml = {
-        "independent_variables": [xAxis, yAxis],
-        "dependent_variables": [{"header": {"name": "SIG", "units": "FB"}, "qualifiers": qualifiers, "values": []}]
-    }
-    h2yaml["independent_variables"][0]["values"] = data[0]
-    h2yaml["independent_variables"][1]["values"] = data[1]
-    h2yaml["dependent_variables"][0]["values"] = data[2]
-    return h2yaml
-
-def convertToYaml(cfg, section, output):
+def convertToYaml(cfg, section, convert_to_fb=False):
     xmin = cfg.getfloat(section, "xmin")
     xmax = cfg.getfloat(section, "xmax")
     ymin = cfg.getfloat(section, "ymin")
@@ -90,33 +112,33 @@ def convertToYaml(cfg, section, output):
         {"name": "INTEGRATED LUMINOSITY", "value": cfg.getfloat(section, "int_lumi"), "units": "1/FB"},
     ]
 
-    h2 = getFromFile(infile, cfg.get(section, "obs_hist"))
-    if h2:
-        with open(section+"_h2.yaml", "w") as f:
-            h2yaml = th2_to_yaml(th2_to_data(h2, xmin, xmax, ymin, ymax), xAxis, yAxis, qualifiers)
-            yaml.dump(h2yaml, f)
+    for name in cfg.get(section, "objects").split(","):
+        name = name.strip()
+        obj = getFromFile(infile, name)
+        if not obj: continue
+        if isinstance(obj, ROOT.TH2):
+            yaml_obj = th2_to_yaml(th2_to_data(obj, xmin, xmax, ymin, ymax), xAxis, yAxis, qualifiers, convert_to_fb)
+        elif isinstance(obj, ROOT.TGraph2D):
+            yaml_obj = tgraph2d_to_yaml(tgraph2d_to_data(obj, xmin, xmax, ymin, ymax), xAxis, yAxis, qualifiers, convert_to_fb)
+        elif isinstance(obj, ROOT.TGraph):
+            yaml_obj = tgraph_to_yaml(tgraph_to_data(obj), xAxis, yAxis, qualifiers)
+        else:
+            print "Do not know what to do with class", type(obj)
 
-    gr_obs = getFromFile(infile, cfg.get(section, "gr_obs"))
-    if gr_obs:
-        with open(section+"_obs.yaml", "w") as f:
-            obs_yaml = tgraph_to_yaml(tgraph_to_data(gr_obs), xAxis, yAxis, qualifiers)
-            yaml.dump(obs_yaml, f)
-
-    gr_exp = getFromFile(infile, cfg.get(section, "gr_exp"))
-    if gr_exp:
-        with open(section+"_exp.yaml", "w") as f:
-            exp_yaml = tgraph_to_yaml(tgraph_to_data(gr_exp), xAxis, yAxis, qualifiers)
-            yaml.dump(exp_yaml, f)
+        yamlName = "{}_{}.yaml".format(section,name)
+        with open(yamlName, "w") as f:
+            print "Write to", yamlName
+            yaml.dump(yaml_obj, f)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Converts limit plots to YAML (for HEPData).')
     parser.add_argument('cfgFile', nargs='+', default=["config.cfg"],
                         help="Configuration files")
-    parser.add_argument('--out', default="out.xyz", help="Output file name")
+    parser.add_argument('--fb', action='store_true', help="Input and output cross sections are assumed to be pb. If you want to convert to fb, use this option")
     args = parser.parse_args()
 
     cfg = ConfigParser.SafeConfigParser()
     cfg.read(args.cfgFile)
 
     for section in cfg.sections():
-        convertToYaml(cfg, section, args.out)
+        convertToYaml(cfg, section, args.fb)
